50d9a30aa88855604e985cf515fa32a9c9a01758
VeronikaBel <157049993+VeronikaBel@users.noreply.github.com>
2024-02-15 14:08:50 +0200

    the second commit
    добавила магические методы сравнения и две функции из задачи 4

--- main.py
+++ main.py
@@ -1,87 +1,182 @@
 class Student:
-    def __init__(self, name, surname, gender):
-        self.name = name
-        self.surname = surname
-        self.gender = gender
-        self.finished_courses = []
-        self.courses_in_progress = []
-        self.grades = {}
+  def __init__(self, name, surname, gender):
+      self.name = name
+      self.surname = surname
+      self.gender = gender
+      self.finished_courses = []
+      self.courses_in_progress = []
+      self.grades = {}
+    
 
-    def _avg_grade(self):
-        if not self.grades:
-            return 0
-        grades_list = []
-        for g in self.grades.values():
-            grades_list.extend(g)
-        return round(sum(grades_list) / len(grades_list), 2)
+  def _avg_grade(self):
+    if not self.grades:
+      return 0
+    grades_list = []
+    for g in self.grades.values():
+      grades_list.extend(g)
+    return round(sum(grades_list) / len(grades_list), 2)
+  
+  def rate_lecturer(self, lecturer, course, grade):
+    if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+      if course in lecturer.grades:
+        lecturer.grades[course] += [grade]
+      else:
+        lecturer.grades[course] = [grade]
+    else:
+      return 'Ошибка'
 
-    def rate_lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer,
-                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
-            if course in lecturer.grades:
-                lecturer.grades[course] += [grade]
-            else:
-                lecturer.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+  def __eq__ (self, other):
+    if isinstance (other, Student):
+      return self._avg_grade() == other._avg_grade()
+    else:
+      print ('Не студент')
 
-    def __str__(self):
-        return f'Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за домашние задания: {self._avg_grade()}\n Курсы в процессе изучения: {self.courses_in_progress}\n Завершенные курсы: {self.finished_courses}'
+  def __ne__ (self, other):
+    if isinstance (other, Student):
+      return self._avg_grade() != other._avg_grade()
+    else:
+      print ('Не студент')
 
-    def best_student(self, other):
-        if not isinstance(other, Student):
-            print('Not a Student')
-        else:
-            return self._avg_grade() < other._avg_grade()
+  def __lt__ (self, other):
+    if isinstance (other, Student):
+      return self._avg_grade() < other._avg_grade()
+    else:
+      print ('Не студент')
 
+  def __le__ (self, other):
+    if isinstance (other, Student):
+      return self._avg_grade() <= other._avg_grade()
+    else:
+      print ('Не студент')
 
+  def __gt__ (self, other):
+    if isinstance (other, Student):
+      return self._avg_grade() > other._avg_grade()
+    else:
+      print ('Не студент')
+
+  def __ge__ (self, other):
+    if isinstance (other, Student):
+      return self._avg_grade() >= other._avg_grade()
+    else:
+      print ('Не студент')
+      
+  def __str__ (self):
+    return f'Имя: {self.name}\n Фамилия: {self.surname}\n Средняя оценка за домашние задания: {self._avg_grade()}\n Курсы в процессе изучения: {self.courses_in_progress}\n Завершенные курсы: {self.finished_courses}'
+
+  def best_student(self, other):
+    if not isinstance(other, Student):
+      print('Не студент')
+    else:
+      return self._avg_grade() < other._avg_grade()
+
+  def avg_grades_homework (students, course):
+    total_grades = 0
+    total_students = []
+    for student in students:
+      if course in student.couses_in_progress:
+        total_grades += sum (student.grades[course])
+        total_students += len (total.studens[course])
+
+    if total_grades == 0 or total_students == 0:
+      return 0  
+
+    return round (total_grades / total_students, 2)
+
+    
 class Mentor:
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-
-    def __str__(self):
-        return f'Имя: {self.name}\nФамилия: {self.surname}'
+  def __init__(self, name, surname):
+      self.name = name
+      self.surname = surname
+      self.courses_attached = []
+  def __str__ (self):
+    return f'Имя: {self.name}\nФамилия: {self.surname}'
 
 
 class Lecturer(Mentor):
+  
+  def __init__(self, name, surname):
+      super().__init__(name, surname)
+      self.grades = {}
 
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        self.grades = {}
+  def _avg_grade(self):
+    if not self.grades:
+      return 0
+    grades_list = []
+    for g in self.grades.values():
+      grades_list.extend(g)
+    return round(sum(grades_list) / len(grades_list), 2)
+      
+  def __str__(self):
+    return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self._avg_grade()}'
 
-    def _avg_grade(self):
-        if not self.grades:
-            return 0
-        grades_list = []
-        for g in self.grades.values():
-            grades_list.extend(g)
-        return round(sum(grades_list) / len(grades_list), 2)
+  def best_lecturer(self, other):
+    if not isinstance(other, Lecturer):
+      print('Не лектор!')
+      return
+    return self._avg_grade() < other._avg_grade()
 
-    def __str__(self):
-        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self._avg_grade()}'
+  def __eq__(self, other):
+    if isinstance(other, Lecturer):
+      return self._avg_grade() == other._avg_grade()
+    else:
+      print('Не лектор!')
 
-    def best_lecturer(self, other):
-        if not isinstance(other, Lecturer):
-            print('Не лектор!')
-            return
-        return self._avg_grade() < other._avg_grade()
+  def __ne__(self,other):
+    if isinstance(other, Lecturer):
+      return self._avg_grade() != other._avg_grade()
+    else:
+      print('Не лектор!')
 
+  def __lt__(self, other):
+    if isinstance(other, Lecturer):
+      return self._avg_grade() < other._avg_grade()
+    else:
+      print('Не лектор!')
+
+  def __gt__(self, other):
+    if isinstance(other, Lecturer):
+      return self._avg_grade() > other._avg_grade()
+    else:
+      print('Не лектор!')
+
+  def __le__(self, other):
+    if isinstance(other, Lecturer):
+      return self._avg_grade() <= other._avg_grade()
+    else:
+      print('Не лектор!')
+
+  def __ge__(self, other):
+    if isinstance(other, Lecturer):
+      return self._avg_grade() >= other._avg_grade()
+    else:
+      print('Не лектор!')
+
+  def avg_grades_lectors (lectors,course):
+    total_grades = 0
+    total_lectors = []
+    for lecturer in lectors:
+      if course in lecturer.courses_attached:
+        total_grades += sum (lecturer.grades[course])
+        total_lectors += len (total.lectors[course])
+    if total_grades == 0 or total_lectors == 0:
+      return 0
+    return round (total_grades / total_lectors, 2)
+    
 
 class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
+  def __init__(self, name, surname):
+    super().__init__(name, surname)
+    
+  def rate_hw(self, student, course, grade):
+      if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+          if course in student.grades:
+              student.grades[course] += [grade]
+          else:
+              student.grades[course] = [grade]
+      else:
+          return 'Ошибка'
+      
 
 student_1 = Student('Ruoy', 'Eman', 'male')
 student_2 = Student('Kate', 'Ryan', 'female')
@@ -116,6 +211,3 @@ lecturer_1.best_lecturer(lecturer_2)
 
 reviewer_1.rate_hw(student_1, 'Python', 10)
 reviewer_2.rate_hw(student_2, 'Java', 8)
-
-
-
